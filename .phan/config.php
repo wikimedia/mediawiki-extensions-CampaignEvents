f42211feb0fcd4462d1dfeed5fd908836f83fc4d
Daimona Eaytoy <daimona.wiki@gmail.com>
2022-01-12 15:59:59 +0100

    Add "Event" namespace
    Bug: T296280
    Change-Id: I1a169013a3e09913aeb1e9bfb641eb8473932b4b

--- .phan/config.php
+++ .phan/config.php
@@ -5,5 +5,6 @@ declare( strict_types=1 );
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
 $cfg['file_list'][] = 'CampaignEvents.alias.php';
+$cfg['file_list'][] = 'CampaignEvents.namespaces.php';
 
 return $cfg;
