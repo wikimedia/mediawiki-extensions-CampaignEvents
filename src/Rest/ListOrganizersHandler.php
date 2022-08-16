d9fa3868be73486787536c1ef2facc8137fa3cc4
Daimona Eaytoy <daimona.wiki@gmail.com>
2022-08-16 15:03:29 +0200

    Adjust visibility of suppressed users
    Fix 500 error in the list participants endpoint; hide the user ID in the
    list organizers endpoint.
    
    Bug: T312772
    Change-Id: I2d014ae26951274d46f33b04c64ae0fa04445128

--- src/Rest/ListOrganizersHandler.php
+++ src/Rest/ListOrganizersHandler.php
@@ -5,6 +5,7 @@ declare( strict_types=1 );
 namespace MediaWiki\Extension\CampaignEvents\Rest;
 
 use MediaWiki\Extension\CampaignEvents\Event\Store\IEventLookup;
+use MediaWiki\Extension\CampaignEvents\MWEntity\CampaignsCentralUserLookup;
 use MediaWiki\Extension\CampaignEvents\Organizers\OrganizersStore;
 use MediaWiki\Extension\CampaignEvents\Organizers\RoleFormatter;
 use MediaWiki\Rest\Response;
@@ -22,20 +23,25 @@ class ListOrganizersHandler extends SimpleHandler {
 	private $organizersStore;
 	/** @var RoleFormatter */
 	private $roleFormatter;
+	/** @var CampaignsCentralUserLookup */
+	private $centralUserLookup;
 
 	/**
 	 * @param IEventLookup $eventLookup
 	 * @param OrganizersStore $organizersStore
 	 * @param RoleFormatter $roleFormatter
+	 * @param CampaignsCentralUserLookup $centralUserLookup
 	 */
 	public function __construct(
 		IEventLookup $eventLookup,
 		OrganizersStore $organizersStore,
-		RoleFormatter $roleFormatter
+		RoleFormatter $roleFormatter,
+		CampaignsCentralUserLookup $centralUserLookup
 	) {
 		$this->eventLookup = $eventLookup;
 		$this->organizersStore = $organizersStore;
 		$this->roleFormatter = $roleFormatter;
+		$this->centralUserLookup = $centralUserLookup;
 	}
 
 	/**
@@ -48,8 +54,12 @@ class ListOrganizersHandler extends SimpleHandler {
 		$organizers = $this->organizersStore->getEventOrganizers( $eventID, self::RES_LIMIT );
 		$respVal = [];
 		foreach ( $organizers as $organizer ) {
+			$user = $organizer->getUser();
+			if ( !$this->centralUserLookup->existsAndIsVisible( $user ) ) {
+				continue;
+			}
 			$respVal[] = [
-				'user_id' => $organizer->getUser()->getCentralID(),
+				'user_id' => $user->getCentralID(),
 				// TODO Should these be localized? It doesn't seem possible right now anyway (T269492)
 				'roles' => array_map( [ $this->roleFormatter, 'getDebugName' ], $organizer->getRoles() )
 			];
