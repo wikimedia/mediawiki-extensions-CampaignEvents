cd3d15358daf5e139ee1e150b40d6c329e84c3f9
Daimona Eaytoy <daimona.wiki@gmail.com>
2023-02-14 23:48:16 +0100

    Add E2E API test for enabling an event registration
    Change-Id: I0a80d4bc45d8aa5d5931659ccc4419b636a1e27f

--- tests/api-testing/CreateRegistration.js
+++ tests/api-testing/CreateRegistration.js
@@ -1,28 +1,69 @@
 'use strict';
 
-const { action, assert, REST } = require( 'api-testing' );
+const { action, assert, REST, clientFactory, utils } = require( 'api-testing' );
 
 describe( 'POST /campaignevents/v0/event_registration', () => {
-	const client = new REST( 'rest.php/campaignevents/v0/event_registration' );
-	let reqBody;
+	let anonClient, organizerClient, blockedUserClient,
+		anonToken, organizerToken, blockedUserToken,
+		eventPage;
 
 	before( async () => {
-		reqBody = {
-			name: 'Some registration',
-			event_page: 'Some page',
+		const organizerUser = await action.root();
+		organizerToken = await organizerUser.token();
+
+		const anonUser = action.getAnon();
+		anonToken = await anonUser.token();
+
+		const blockedUser = await action.user( 'Blocked' );
+		await organizerUser.action( 'block', {
+			user: blockedUser.username,
+			reason: 'Test block',
+			token: organizerToken
+		}, 'POST' );
+		blockedUserToken = await blockedUser.token();
+
+		anonClient = new REST( 'rest.php/campaignevents/v0/event_registration' );
+		organizerClient = clientFactory.getRESTClient( 'rest.php/campaignevents/v0/event_registration', organizerUser );
+		blockedUserClient = clientFactory.getRESTClient( 'rest.php/campaignevents/v0/event_registration', blockedUser );
+
+		eventPage = utils.title( 'Event:Event page ' );
+		await organizerUser.edit( eventPage, {} );
+	} );
+
+	function getBody( token ) {
+		return {
+			event_page: eventPage,
 			chat_url: 'https://example.org',
 			timezone: 'UTC',
-			start_time: '20200220200220',
-			end_time: '20200220200222',
+			start_time: '30200220200220',
+			end_time: '30200220200222',
 			type: 'generic',
 			online_meeting: true,
-			token: await action.getAnon().token()
+			token: token
 		};
+	}
+
+	describe( 'permission error', () => {
+		it( 'fails session check for anonymous users', async () => {
+			const { body: sourceBody } = await anonClient.post( '', getBody( anonToken ) );
+			assert.strictEqual( sourceBody.httpCode, 403 );
+			assert.strictEqual( sourceBody.errorKey, 'rest-badtoken' );
+			assert.property( sourceBody, 'messageTranslations' );
+			assert.property( sourceBody.messageTranslations, 'en' );
+			assert.include( sourceBody.messageTranslations.en, 'no session' );
+		} );
+		it( 'fails for a blocked user', async () => {
+			const { body: sourceBody } = await blockedUserClient.post( '', getBody( blockedUserToken ) );
+			assert.strictEqual( sourceBody.httpCode, 403 );
+			assert.property( sourceBody, 'messageTranslations' );
+			assert.property( sourceBody.messageTranslations, 'en' );
+			assert.include( sourceBody.messageTranslations.en, 'not allowed' );
+		} );
 	} );
 
 	describe( 'param validation', () => {
 		it( 'fails if no parameters were given', async () => {
-			const { body: sourceBody } = await client.post( '' );
+			const { body: sourceBody } = await organizerClient.post( '' );
 			assert.strictEqual( sourceBody.httpCode, 400 );
 			assert.property( sourceBody, 'messageTranslations' );
 			assert.property( sourceBody.messageTranslations, 'en' );
@@ -30,14 +71,12 @@ describe( 'POST /campaignevents/v0/event_registration', () => {
 		} );
 	} );
 
-	describe( 'permission error', () => {
-		it( 'fails session check for anonymous users', async () => {
-			const { body: sourceBody } = await client.post( '', reqBody );
-			assert.strictEqual( sourceBody.httpCode, 403 );
-			assert.strictEqual( sourceBody.errorKey, 'rest-badtoken' );
-			assert.property( sourceBody, 'messageTranslations' );
-			assert.property( sourceBody.messageTranslations, 'en' );
-			assert.include( sourceBody.messageTranslations.en, 'no session' );
+	describe( 'successful', () => {
+		it( 'succeeds for an authorized user if the request body is valid', async () => {
+			const { status: statusCode, body: sourceBody } = await organizerClient.post( '', getBody( organizerToken ) );
+			assert.strictEqual( statusCode, 201 );
+			assert.property( sourceBody, 'id' );
+			assert.isNumber( sourceBody.id );
 		} );
 	} );
 } );
