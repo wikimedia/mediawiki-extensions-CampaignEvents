564be8603952665e964ea1395e13a1d761a851df
Vaughn Walters <vwalters@wikimedia.org>
2023-03-21 15:42:18 -0500

    selenium: Refactor edit event registration test
    Bug: T316875
    Change-Id: I06814c78218d10ef54feedb728fc79459f1185e3

--- tests/selenium/specs/editEventRegistration.js
+++ tests/selenium/specs/editEventRegistration.js
@@ -4,31 +4,35 @@ const assert = require( 'assert' ),
 	EventPage = require( '../pageobjects/event.page' ),
 	EventRegistrationPage = require( '../pageobjects/eventRegistration.page' ),
 	LoginPage = require( 'wdio-mediawiki/LoginPage' ),
-	Rest = require( '../pageobjects/rest.page' ),
-	event = EventRegistrationPage.getTestString( 'Event:e2e' ),
-	updatedEventPage = EventRegistrationPage.getTestString( 'Event:updatede2e' );
+	Rest = require( '../pageobjects/rest.page' );
+
+let event,
+	id;
 
 describe( 'Edit Event Registration', function () {
 
-	it( 'can allow organizer to update event data', async function () {
+	beforeEach( async function () {
+		event = EventRegistrationPage.getTestString( 'Event:e2e' );
 		await LoginPage.loginAdmin();
 		await EventRegistrationPage.createEvent( event );
+		id = await Rest.enableEvent( event );
+	} );
+
+	it( 'can allow organizer to update event data', async function () {
+		const updatedEventPage = EventRegistrationPage.getTestString( 'Event:updatede2e' );
 		await EventRegistrationPage.createEvent( updatedEventPage );
-		const id = await Rest.enableEvent( event );
+
 		await EventRegistrationPage.editEvent( {
 			event: updatedEventPage,
 			id,
 			start: { year: 2099, day: 12 },
 			end: { year: 2100, day: 14 }
 		} );
+
 		assert.deepEqual( await EventRegistrationPage.feedback.getText(), 'The registration was edited. See event page.' );
 	} );
 
 	it( 'can allow organizer to change the event to be in person', async function () {
-		await LoginPage.loginAdmin();
-		await EventRegistrationPage.createEvent( event );
-		const id = await Rest.enableEvent( event );
-
 		await EventRegistrationPage.editEvent( {
 			id,
 			meetingType: 'inperson'
