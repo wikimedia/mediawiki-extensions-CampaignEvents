470d6f76f8a88a92cb5ed2d3debe872372cd698d
Vaughn Walters <vwalters@wikimedia.org>
2023-03-22 08:25:42 -0500

    selenium: Edit Event Registration can allow organizer to change the event to be online and in-person
    Bug: T316875
    Change-Id: I44f2d6c236187e25a2739d1e5b9c45288800a5ec

--- tests/selenium/specs/editEventRegistration.js
+++ tests/selenium/specs/editEventRegistration.js
@@ -41,4 +41,14 @@ describe( 'Edit Event Registration', function () {
 		EventPage.open( event );
 		assert.deepEqual( await EventPage.eventType.getText(), 'In-person event' );
 	} );
+
+	it( 'can allow organizer to change the event to be online and in-person', async function () {
+		await EventRegistrationPage.editEvent( {
+			id,
+			meetingType: 'hybrid'
+		} );
+
+		EventPage.open( event );
+		assert.deepEqual( await EventPage.eventType.getText(), 'Online and in-person event' );
+	} );
 } );
