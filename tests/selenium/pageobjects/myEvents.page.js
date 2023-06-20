2d062d3a2d9908cf6e09549d1b9166f3a208f1af
Daimona Eaytoy <daimona.wiki@gmail.com>
2023-06-20 19:42:24 +0200

    Try to make the myEvents selenium spec more robust
    This spec is failing relatively often, see for instance all the failed
    gate-and-submit attempts in I3bcc1654a181444619735d6c07b83b2aa9569624.
    
    I'm not really sure why that's the case, but at least this patch makes
    the spec more robust by filtering for open events first, so as to make
    sure that the first event in the list is open, and that it can therefore
    be closed.
    
    Change-Id: I70d5a1e1a563095dc4310143f5323c325c62a8d7

--- tests/selenium/pageobjects/myEvents.page.js
+++ tests/selenium/pageobjects/myEvents.page.js
@@ -10,21 +10,31 @@ class MyEventsPage extends Page {
 	get notification() { return $( '.mw-notification' ); }
 	get firstEvent() { return $( '.ext-campaignevents-eventspager-eventpage-link' ); }
 	get deleteConfirmationButton() { return $( '.oo-ui-messageDialog' ).$( '.oo-ui-buttonElement-button=Delete' ); }
+	get filterMenu() { return $( '.ext-campaignevents-myevents-filter-widget > .oo-ui-buttonElement' ); }
+	get eventStatusFilter() { return $( '#mw-input-wpStatus' ); }
+	get openEventsFilter() { return $( '.oo-ui-defaultOverlay .oo-ui-selectWidget' ).$( '.oo-ui-optionWidget=Open events' ); }
+	get filtersSubmitButton() { return $( '#ext-campaignevents-myevents-form button[type="submit"]' ); }
 
 	open() {
 		super.openTitle( 'Special:MyEvents' );
 	}
 
-	async closeRegistration() {
+	async filterByOpenRegistrations() {
+		await this.filterMenu.click();
+		await this.eventStatusFilter.click();
+		await this.openEventsFilter.click();
+		await this.filtersSubmitButton.click();
+	}
+
+	async closeFirstRegistration() {
 		await this.manageEventMenuButton.click();
 		await this.closeRegistrationButton.click();
 	}
 
-	async deleteRegistration() {
+	async deleteFirstRegistration() {
 		await this.manageEventMenuButton.click();
 		await this.deleteRegistrationButton.click();
 		await this.deleteConfirmationButton.click();
-
 	}
 }
 
