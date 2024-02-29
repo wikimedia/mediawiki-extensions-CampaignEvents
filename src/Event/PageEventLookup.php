f5fc0770449564dc96ae9c4886b79cfc53a665d6
Daimona Eaytoy <daimona.wiki@gmail.com>
2024-02-29 16:33:20 +0100

    Make translated event pages use the same registration as the source page
    Any subpage of a translatable page, being just a translation, should
    still use the event registration of the source (parent) page.
    
    This is achieved by canonicalizing the given page in PageEventLookup
    before hitting the DB.
    
    Note that this can only be done if the given page is local, because
    Translate doesn't support checking foreign pages. This should not be a
    problem in practice.
    
    Also adjust some typehints to use PageIdentity instead of
    ProperPageIdentity, as the latter isn't widely used in typehints, but
    they're pretty much the same thing.
    
    Bug: T357716
    Depends-On: Ib03ed861d95f5c042ed63291ef539d84b7db7077
    Change-Id: I6d90fa3ee7969b48ecd1fea2afc7c9123c621e74

--- src/Event/PageEventLookup.php
+++ src/Event/PageEventLookup.php
@@ -5,12 +5,17 @@ declare( strict_types=1 );
 namespace MediaWiki\Extension\CampaignEvents\Event;
 
 use IDBAccessObject;
+use MediaWiki\DAO\WikiAwareEntity;
 use MediaWiki\Extension\CampaignEvents\Event\Store\EventNotFoundException;
 use MediaWiki\Extension\CampaignEvents\Event\Store\IEventLookup;
 use MediaWiki\Extension\CampaignEvents\MWEntity\CampaignsPageFactory;
 use MediaWiki\Extension\CampaignEvents\MWEntity\ICampaignsPage;
+use MediaWiki\Extension\CampaignEvents\MWEntity\MWPageProxy;
+use MediaWiki\Extension\Translate\PageTranslation\TranslatablePage;
 use MediaWiki\Linker\LinkTarget;
 use MediaWiki\Page\PageIdentity;
+use MediaWiki\Title\TitleFactory;
+use RuntimeException;
 
 /**
  * This class is responsible for finding the event registration associated with a given page. This includes
@@ -21,17 +26,25 @@ class PageEventLookup {
 
 	private IEventLookup $eventLookup;
 	private CampaignsPageFactory $campaignsPageFactory;
+	private TitleFactory $titleFactory;
+	private bool $isTranslateExtensionInstalled;
 
 	/**
 	 * @param IEventLookup $eventLookup
 	 * @param CampaignsPageFactory $campaignsPageFactory
+	 * @param TitleFactory $titleFactory
+	 * @param bool $isTranslateExtensionInstalled
 	 */
 	public function __construct(
 		IEventLookup $eventLookup,
-		CampaignsPageFactory $campaignsPageFactory
+		CampaignsPageFactory $campaignsPageFactory,
+		TitleFactory $titleFactory,
+		bool $isTranslateExtensionInstalled
 	) {
 		$this->eventLookup = $eventLookup;
 		$this->campaignsPageFactory = $campaignsPageFactory;
+		$this->titleFactory = $titleFactory;
+		$this->isTranslateExtensionInstalled = $isTranslateExtensionInstalled;
 	}
 
 	/**
@@ -43,6 +56,7 @@ class PageEventLookup {
 		$page,
 		int $readFlags = IDBAccessObject::READ_NORMAL
 	): ?ExistingEventRegistration {
+		$page = $this->getCanonicalPage( $page );
 		if ( $page->getNamespace() !== NS_EVENT ) {
 			return null;
 		}
@@ -56,6 +70,14 @@ class PageEventLookup {
 	}
 
 	public function getRegistrationForPage( ICampaignsPage $page ): ?ExistingEventRegistration {
+		if ( !$page instanceof MWPageProxy ) {
+			throw new RuntimeException( 'Unexpected ICampaignsPage implementation.' );
+		}
+		$pageIdentity = $page->getPageIdentity();
+		$canonicalPageIdentity = $this->getCanonicalPage( $pageIdentity );
+		if ( $canonicalPageIdentity !== $pageIdentity ) {
+			$page = $this->campaignsPageFactory->newFromLocalMediaWikiPage( $canonicalPageIdentity );
+		}
 		if ( $page->getNamespace() !== NS_EVENT ) {
 			return null;
 		}
@@ -65,5 +87,24 @@ class PageEventLookup {
 		} catch ( EventNotFoundException $_ ) {
 			return null;
 		}
+	}
+
+	/**
+	 * @param PageIdentity|LinkTarget $page
+	 * @return PageIdentity
+	 */
+	private function getCanonicalPage( $page ): PageIdentity {
+		// XXX: Can't canonicalize foreign pages.
+		if ( $this->isTranslateExtensionInstalled && $page->getWikiId() === WikiAwareEntity::LOCAL ) {
+			$title = $page instanceof PageIdentity
+				? $this->titleFactory->newFromPageIdentity( $page )
+				: $this->titleFactory->newFromLinkTarget( $page );
+			$transPage = TranslatablePage::isTranslationPage( $title );
+			if ( $transPage ) {
+				// If this is a translation subpage, look up the source page instead (T357716)
+				return $transPage->getPageIdentity();
+			}
+		}
+		return $page;
 	}
 }
--- tests/phpunit/integration/Event/PageEventLookupTest.php
+++ tests/phpunit/integration/Event/PageEventLookupTest.php
@@ -4,23 +4,33 @@ declare( strict_types=1 );
 
 namespace MediaWiki\Extension\CampaignEvents\Tests\Integration\Event;
 
+use DateTimeZone;
 use MediaWiki\DAO\WikiAwareEntity;
+use MediaWiki\Extension\CampaignEvents\CampaignEventsServices;
+use MediaWiki\Extension\CampaignEvents\Event\EventRegistration;
 use MediaWiki\Extension\CampaignEvents\Event\PageEventLookup;
 use MediaWiki\Extension\CampaignEvents\Event\Store\IEventLookup;
 use MediaWiki\Extension\CampaignEvents\MWEntity\CampaignsPageFactory;
 use MediaWiki\Extension\CampaignEvents\MWEntity\MWPageProxy;
+use MediaWiki\Extension\Translate\PageTranslation\TranslatablePage;
+use MediaWiki\MediaWikiServices;
 use MediaWiki\Page\PageIdentityValue;
+use MediaWiki\Title\Title;
+use MediaWiki\Title\TitleFactory;
 use MediaWikiIntegrationTestCase;
 
 /**
  * @covers \MediaWiki\Extension\CampaignEvents\Event\PageEventLookup
- * @todo Make this a unit test once it's possible to use namespace constants (T310375)
+ * @group Database
+ * @todo The Translate tests aren't run in CI due to T358985
  */
 class PageEventLookupTest extends MediaWikiIntegrationTestCase {
 	private function getLookup( IEventLookup $eventLookup = null ): PageEventLookup {
 		return new PageEventLookup(
 			$eventLookup ?? $this->createMock( IEventLookup::class ),
-			$this->createMock( CampaignsPageFactory::class )
+			$this->createMock( CampaignsPageFactory::class ),
+			$this->createMock( TitleFactory::class ),
+			false
 		);
 	}
 
@@ -30,10 +40,70 @@ class PageEventLookupTest extends MediaWikiIntegrationTestCase {
 		$this->assertNull( $this->getLookup( $eventLookup )->getRegistrationForLocalPage( $page ) );
 	}
 
+	public function testGetRegistrationForLocalPage__translatablePage() {
+		$this->markTestSkippedIfExtensionNotLoaded( 'Translate' );
+		[ $translationSubpage, $eventID ] = $this->createTranslationPageWithEventAndSubpage();
+		$pageEventLookup = CampaignEventsServices::getPageEventLookup();
+		$event = $pageEventLookup->getRegistrationForLocalPage( $translationSubpage );
+		$this->assertNotNull( $event );
+		$this->assertSame( $eventID, $event->getID() );
+	}
+
 	public function testGetRegistrationForPage__notInEventNamespace() {
 		$page = new PageIdentityValue( 42, NS_PROJECT, 'Some_title', WikiAwareEntity::LOCAL );
 		$pageProxy = new MWPageProxy( $page, 'DoesNotMatter' );
 		$eventLookup = $this->createNoOpMock( IEventLookup::class );
 		$this->assertNull( $this->getLookup( $eventLookup )->getRegistrationForPage( $pageProxy ) );
+	}
+
+	public function testGetRegistrationForPage__translatablePage() {
+		$this->markTestSkippedIfExtensionNotLoaded( 'Translate' );
+		[ $translationSubpage, $eventID ] = $this->createTranslationPageWithEventAndSubpage();
+		$pageEventLookup = CampaignEventsServices::getPageEventLookup();
+		$campaignsTranslationSubpage = CampaignEventsServices::getPageFactory()
+			->newFromLocalMediaWikiPage( $translationSubpage );
+		$event = $pageEventLookup->getRegistrationForPage( $campaignsTranslationSubpage );
+		$this->assertNotNull( $event );
+		$this->assertSame( $eventID, $event->getID() );
+	}
+
+	/**
+	 * @return array With [ translation subpage, event ID ]
+	 */
+	private function createTranslationPageWithEventAndSubpage(): array {
+		$title = Title::makeTitle( NS_EVENT, __METHOD__ );
+		$page = $this->getNonexistingTestPage( $title );
+		$translatablePage = TranslatablePage::newFromTitle( $page->getTitle() );
+		$editStatus = $this->editPage( $page, '<translate>Foo</translate>' );
+		$translatablePage->addMarkedTag( $editStatus->getNewRevision()->getId() );
+
+		$sourceLang = $translatablePage->getMessageGroup()->getSourceLanguage();
+		$transLang = $sourceLang !== 'it' ? 'it' : 'en';
+		$transTitle = $title->getSubpage( $transLang );
+		$subpage = MediaWikiServices::getInstance()->getWikiPageFactory()->newFromTitle( $transTitle );
+
+		$event = new EventRegistration(
+			null,
+			$page->getTitle()->getText(),
+			new MWPageProxy( $page, $page->getTitle()->getPrefixedText() ),
+			null,
+			[],
+			EventRegistration::STATUS_OPEN,
+			new DateTimeZone( 'UTC' ),
+			'20240229120000',
+			'20240301120000',
+			EventRegistration::TYPE_GENERIC,
+			EventRegistration::MEETING_TYPE_ONLINE,
+			null,
+			null,
+			null,
+			[],
+			null,
+			null,
+			null
+		);
+		$eventID = CampaignEventsServices::getEventStore()->saveRegistration( $event );
+
+		return [ $subpage, $eventID ];
 	}
 }
