d824b5cce86f8b5797ffd0566d27b50b2515ba9f
Daimona Eaytoy <daimona.wiki@gmail.com>
2024-04-24 19:13:58 +0200

    Exclude Translate's Echo stubs from phan
    Currently causing phan to fail on master because we already have Echo as
    a dependency, but Translate brings in its own Echo stubs (added in
    I7a9246dce8fe).
    
    Change-Id: I3d665bafc39d72edb801ad471f72a77c546d3a17

--- .phan/config.php
+++ .phan/config.php
@@ -23,6 +23,17 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 $cfg['file_list'][] = 'CampaignEvents.alias.php';
 $cfg['file_list'][] = 'CampaignEvents.namespaces.php';
 
+$cfg['exclude_file_list'] = array_merge(
+	$cfg['exclude_file_list'],
+	[
+		'../../extensions/Translate/.phan/stubs/BeforeCreateEchoEventHook.php',
+		'../../extensions/Translate/.phan/stubs/EchoAttributeManager.php',
+		'../../extensions/Translate/.phan/stubs/EchoEventPresentationModel.php',
+		'../../extensions/Translate/.phan/stubs/EchoGetBundleRulesHook.php',
+		'../../extensions/Translate/.phan/stubs/Event.php',
+	]
+);
+
 $cfg['plugins'] = array_merge( $cfg['plugins'], [
 	'StrictComparisonPlugin',
 	'StrictLiteralComparisonPlugin',
