a7a4b82b97de175ae3b39f646c3956f2be58838a
Daimona Eaytoy <daimona.wiki@gmail.com>
2024-11-06 13:12:38 +0100

    Add internal REST endpoint to format date strings
    This allows us to format a date-time according to the user preferences,
    in all the formats supported by Language::sprintfDate(). As discussed in
    T21992, this functionality is not exposed in JS (as the PHP
    date-formatting logic is quite complex and supports loads of languages).
    
    So, introduce an internal REST endpoint to provide indirect access to
    sprintfDate in the frontend.
    
    Bug: T362639
    Change-Id: I2b1f0363a2c6cc7f22dc01bd6cfd0ef18cc32ae5

---
+++ tests/phpunit/integration/Rest/GetFormattedTimeHandlerTest.php
@@ -0,0 +1,151 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace MediaWiki\Extension\CampaignEvents\Tests\Integration\Rest;
+
+use Generator;
+use MediaWiki\Extension\CampaignEvents\Rest\GetFormattedTimeHandler;
+use MediaWiki\Rest\HttpException;
+use MediaWiki\Rest\RequestData;
+use MediaWiki\Tests\Rest\Handler\HandlerTestTrait;
+use MediaWiki\User\StaticUserOptionsLookup;
+use MediaWiki\User\UserIdentityValue;
+use MediaWikiIntegrationTestCase;
+
+/**
+ * @group Test
+ * @covers \MediaWiki\Extension\CampaignEvents\Rest\GetFormattedTimeHandler
+ */
+class GetFormattedTimeHandlerTest extends MediaWikiIntegrationTestCase {
+	use HandlerTestTrait;
+
+	private const USER_PREFERENCES = [
+		'Kurt' => [ 'date' => 'mdy' ],
+		'Évariste' => [ 'date' => 'dmy' ],
+	];
+	private const DEFAULT_PREFERENCES = [ 'date' => 'ymd' ];
+
+	protected function setUp(): void {
+		parent::setUp();
+		$userOptionsLookup = new StaticUserOptionsLookup(
+			self::USER_PREFERENCES,
+			self::DEFAULT_PREFERENCES
+		);
+		$this->setService( 'UserOptionsLookup', $userOptionsLookup );
+	}
+
+	private function newHandler(): GetFormattedTimeHandler {
+		return new GetFormattedTimeHandler( $this->getServiceContainer()->getLanguageFactory() );
+	}
+
+	/**
+	 * @dataProvider provideRun
+	 */
+	public function testRun(
+		string $language,
+		string $username,
+		string $start,
+		string $end,
+		array $expectedResponse
+	): void {
+		$handler = $this->newHandler();
+		$performer = $this->mockAuthority( new UserIdentityValue( 42, $username ), static fn () => true );
+		$respData = $this->executeHandlerAndGetBodyData(
+			$handler,
+			new RequestData( [ 'pathParams' => [ 'languageCode' => $language, 'start' => $start, 'end' => $end ] ] ),
+			[],
+			[],
+			[],
+			[],
+			$performer
+		);
+		$this->assertSame( $expectedResponse, $respData );
+	}
+
+	public function provideRun(): Generator {
+		$startTS = '20231106123042';
+		$endTS = '20231231235959';
+
+		yield 'English mdy' => [
+			'en',
+			'Kurt',
+			$startTS,
+			$endTS,
+			[
+				'startTime' => '12:30',
+				'startDate' => 'November 6, 2023',
+				'startDateTime' => '12:30, November 6, 2023',
+				'endTime' => '23:59',
+				'endDate' => 'December 31, 2023',
+				'endDateTime' => '23:59, December 31, 2023',
+			]
+		];
+		yield 'English dmy' => [
+			'en',
+			'Évariste',
+			$startTS,
+			$endTS,
+			[
+				'startTime' => '12:30',
+				'startDate' => '6 November 2023',
+				'startDateTime' => '12:30, 6 November 2023',
+				'endTime' => '23:59',
+				'endDate' => '31 December 2023',
+				'endDateTime' => '23:59, 31 December 2023',
+			]
+		];
+		yield 'French dmy' => [
+			'fr',
+			'Évariste',
+			$startTS,
+			$endTS,
+			[
+				'startTime' => '12:30',
+				'startDate' => '6 novembre 2023',
+				'startDateTime' => '6 novembre 2023 à 12:30',
+				'endTime' => '23:59',
+				'endDate' => '31 décembre 2023',
+				'endDateTime' => '31 décembre 2023 à 23:59',
+			]
+		];
+		yield 'French mdy' => [
+			'fr',
+			'Kurt',
+			$startTS,
+			$endTS,
+			[
+				'startTime' => '12:30',
+				'startDate' => 'novembre 6, 2023',
+				'startDateTime' => 'novembre 6, 2023 à 12:30',
+				'endTime' => '23:59',
+				'endDate' => 'décembre 31, 2023',
+				'endDateTime' => 'décembre 31, 2023 à 23:59',
+			]
+		];
+		yield 'German default (ymd)' => [
+			'de',
+			'Carl Friedrich',
+			$startTS,
+			$endTS,
+			[
+				'startTime' => '12:30',
+				'startDate' => '2023 Nov. 6',
+				'startDateTime' => '12:30, 2023 Nov. 6',
+				'endTime' => '23:59',
+				'endDate' => '2023 Dez. 31',
+				'endDateTime' => '23:59, 2023 Dez. 31',
+			]
+		];
+	}
+
+	public function testRun__invalidTimestamp(): void {
+		$handler = $this->newHandler();
+		$this->expectException( HttpException::class );
+		$this->expectExceptionMessage( 'Invalid input timestamp' );
+		$this->executeHandler(
+			$handler,
+			new RequestData( [ 'pathParams' => [ 'languageCode' => 'en', 'start' => 'hot', 'end' => 'garbage' ] ] )
+		);
+	}
+}
