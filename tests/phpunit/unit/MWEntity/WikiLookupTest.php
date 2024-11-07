e82e2c11e93711392de6aaf4c6767308d64d2b8f
Daimona Eaytoy <daimona.wiki@gmail.com>
2024-11-07 15:48:39 +0100

    Introduce WikiLookup
    Essentially a wrapper around $wgConf to get a list of wikis in the wiki
    family, including i18n support when the custom
    `project-localized-name-*` messages are available (for example, in the
    WikimediaMessages extension for WMF wikis).
    
    Bug: T367224
    Change-Id: I14d15cc96daad6d8abd2643687abe65a4c686382

---
+++ tests/phpunit/unit/MWEntity/WikiLookupTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace MediaWiki\Extension\CampaignEvents\Tests\Unit\MWEntity;
+
+use MediaWiki\Config\SiteConfiguration;
+use MediaWiki\Extension\CampaignEvents\MWEntity\WikiLookup;
+use MediaWiki\Tests\Unit\FakeQqxMessageLocalizer;
+use MediaWikiUnitTestCase;
+use Wikimedia\ObjectCache\EmptyBagOStuff;
+use Wikimedia\ObjectCache\WANObjectCache;
+
+/**
+ * @covers \MediaWiki\Extension\CampaignEvents\MWEntity\WikiLookup
+ */
+class WikiLookupTest extends MediaWikiUnitTestCase {
+	private function getLookup( array $localDatabases ): WikiLookup {
+		$siteConfig = $this->createMock( SiteConfiguration::class );
+		$siteConfig->method( 'getLocalDatabases' )->willReturn( $localDatabases );
+		return new WikiLookup(
+			$siteConfig,
+			new WANObjectCache( [ 'cache' => new EmptyBagOStuff() ] ),
+			new FakeQqxMessageLocalizer()
+		);
+	}
+
+	public function testGetAllWikis() {
+		$localWikis = [ 'foowiki', 'barwiki' ];
+		$lookup = $this->getLookup( $localWikis );
+		$this->assertSame( $localWikis, $lookup->getAllWikis() );
+	}
+
+	public function testGetListForSelect() {
+		$localWikis = [ 'foowiki', 'barwiki' ];
+		$lookup = $this->getLookup( $localWikis );
+		$this->assertSame(
+			[ 'foowiki' => '(project-localized-name-foowiki)', 'barwiki' => '(project-localized-name-barwiki)' ],
+			$lookup->getListForSelect()
+		);
+	}
+
+	public function testGetLocalizedNames() {
+		$localWikis = [ 'foowiki', 'barwiki', 'bazwiki' ];
+		$lookup = $this->getLookup( $localWikis );
+		$expectedLocalized = [
+			'foowiki' => '(project-localized-name-foowiki)',
+			'barwiki' => '(project-localized-name-barwiki)'
+		];
+		$this->assertSame( $expectedLocalized, $lookup->getLocalizedNames( array_keys( $expectedLocalized ) ) );
+	}
+}
