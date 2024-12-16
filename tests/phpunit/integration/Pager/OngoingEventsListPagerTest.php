29ddd168d266febb7f9d6280948eb26c1b64fef7
Michelle <mhorsey@wikimedia.org>
2024-12-16 14:56:36 +0000

    Add new pager for ongoing events
    Reuse code from the existing list pager, but reverse the ordering and
    change the query condition so that only ongoing events are returned.
    
    Move the existing test cases to a trait, and separate ongoing vs
    upcoming event tests. The upcoming part is unused for the time being.
    Old tests are retained as legacy, but will be deleted in favour of the
    new "upcoming events" test cases when the existing pager is updated.
    
    Bug: T382246
    Change-Id: I837efd3a72eafdd1072ec3a45e31351c26968381

---
+++ tests/phpunit/integration/Pager/OngoingEventsListPagerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace MediaWiki\Extension\CampaignEvents\Tests\Integration\Pager;
+
+use MediaWiki\Extension\CampaignEvents\CampaignEventsServices;
+use MediaWiki\Extension\CampaignEvents\Event\EventRegistration;
+use MediaWikiIntegrationTestCase;
+
+/**
+ * @group Test
+ * @group Database
+ * @covers \MediaWiki\Extension\CampaignEvents\Pager\OngoingEventsListPager
+ */
+class OngoingEventsListPagerTest extends MediaWikiIntegrationTestCase {
+	use ListPagersTestHelperTrait;
+
+	/**
+	 * @dataProvider provideOngoingDateFilters
+	 */
+	public function testDateFilters(
+		int $searchStart,
+		?int $searchTo,
+		bool $expectsFound
+	): void {
+		$pager = CampaignEventsServices::getEventsPagerFactory()->newOngoingListPager(
+			'',
+			null,
+			wfTimestamp( TS_MW, $searchStart ),
+			[],
+			[]
+		);
+		$this->assertSame( $expectsFound ? 1 : 0, $pager->getNumRows() );
+	}
+
+	public function testCanUseFilters() {
+		$pager = CampaignEventsServices::getEventsPagerFactory()->newOngoingListPager(
+			self::$EVENT_NAME,
+			EventRegistration::MEETING_TYPE_ONLINE_AND_IN_PERSON,
+			wfTimestamp( TS_MW, self::$EVENT_START + 1 ),
+			[ 'any_wiki_name' ],
+			[ self::$EVENT_TOPIC ]
+		);
+		$this->assertSame( 1, $pager->getNumRows() );
+	}
+}
