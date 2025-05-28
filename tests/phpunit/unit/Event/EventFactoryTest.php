5a4fa6a447b0283d3e1de71fbda94542e45d389d
Daimona Eaytoy <daimona.wiki@gmail.com>
2025-05-28 23:20:15 +0200

    Fix event topics-related names in EventFactoryTest
    Update some copy-pasted code named after event wikis.
    
    Fix case for various method calls to match the method definition. PHP
    function and method names are case-insensitive, but that doesn't mean
    the "feature" should be used, as it makes the code less searchable and
    consistent; see open proposal in T381926 to flag these in PHPCS.
    
    Change-Id: Ibd665f0192fdfa0c2e90604e74dcb2046d86809a

--- tests/phpunit/unit/Event/EventFactoryTest.php
+++ tests/phpunit/unit/Event/EventFactoryTest.php
@@ -80,15 +80,15 @@ class EventFactoryTest extends MediaWikiUnitTestCase {
 	}
 
 	/**
-	 * @return string[] A list of wiki IDs that are considered valid within this test class. This is guaranteed to be
-	 * a list of all possible "XYwiki" combinations, and is provided for convenience.
+	 * @return string[] A list of topic IDs that are considered valid within this test class. This is guaranteed to be
+	 * a list of all possible "XYtopic" combinations, and is provided for convenience.
 	 */
 	private static function getValidTopics(): array {
-		$validWikis = [];
+		$validTopics = [];
 		for ( $prefix = 'a'; $prefix !== 'aa'; $prefix++ ) {
-			$validWikis[] = $prefix . 'topic';
+			$validTopics[] = $prefix . 'topic';
 		}
-		return $validWikis;
+		return $validTopics;
 	}
 
 	private function getEventFactory(
@@ -621,7 +621,7 @@ class EventFactoryTest extends MediaWikiUnitTestCase {
 		$args = self::getTestDataWithDefault( [ 'topics' => $topics ] );
 		$event = $this->doTestWithArgs( $args, $expectedErrors );
 		if ( $event ) {
-			$this->assertSame( $expectedTopics, $event->gettopics() );
+			$this->assertSame( $expectedTopics, $event->getTopics() );
 		}
 	}
 
@@ -639,7 +639,7 @@ class EventFactoryTest extends MediaWikiUnitTestCase {
 			[ 'atopic' ]
 		];
 		yield 'Too many topics' => [
-			self::getValidtopics(),
+			self::getValidTopics(),
 			[ 'campaignevents-error-too-many-topics' ]
 		];
 		yield 'Valid and invalid topics' => [
@@ -655,7 +655,7 @@ class EventFactoryTest extends MediaWikiUnitTestCase {
 			[ 'campaignevents-error-invalid-topics' ]
 		];
 		yield 'Invalid and too many topics' => [
-			[ 'doesnotexisttopic', ...self::getValidtopics() ],
+			[ 'doesnotexisttopic', ...self::getValidTopics() ],
 			[ 'campaignevents-error-too-many-topics', 'campaignevents-error-invalid-topics' ]
 		];
 	}
