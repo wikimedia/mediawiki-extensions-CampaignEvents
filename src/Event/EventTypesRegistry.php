6f879db527a85041cd9d14bcaf5e548229776896
Daimona Eaytoy <daimona.wiki@gmail.com>
2025-05-28 02:07:00 +0200

    Refactor EventTypesRegistry
    * Drop "debug name" functionality as it's not used and not planned to be
      used.
    * Do not expose group name localization directly
    * Put all the group and type information inside a single data structure,
      as in ArticleTopicFiltersRegistry for example.
    * Drop constants for individual even type "codenames". These are used in
      tests only, but the actual code should be mostly oblivious to the
      content of these constants. Removing them means we don't have to
      maintain this parallel list. As an exception, keep a constant for the
      "other" type due to its special nature.
    * Make DB values part of the main data structure and clarify their
      purpose. Bring in the respective methods from EventStore and add
      tests.
    
    Change-Id: I84daba4b34a7d701b1d51b6983f91ccb70af6e1d

--- src/Event/EventTypesRegistry.php
+++ src/Event/EventTypesRegistry.php
@@ -12,86 +12,77 @@ class EventTypesRegistry {
 
 	public const SERVICE_NAME = 'CampaignEventsEventTypesRegistry';
 
-	public const EVENT_TYPE_MSG_MAP = [
-		self::EVENT_TYPE_EDITING_EVENT      => 'campaignevents-eventtype-editing-event',
-		self::EVENT_TYPE_MEDIA_UPLOAD_EVENT => 'campaignevents-eventtype-media-upload-event',
-		self::EVENT_TYPE_BACKLOG_DRIVE      => 'campaignevents-eventtype-backlog-drive',
-		self::EVENT_TYPE_CONTEST            => 'campaignevents-eventtype-contest',
-		self::EVENT_TYPE_WORKSHOP           => 'campaignevents-eventtype-workshop',
-		self::EVENT_TYPE_TRAINING           => 'campaignevents-eventtype-training',
-		self::EVENT_TYPE_MEETUP             => 'campaignevents-eventtype-meetup',
-		self::EVENT_TYPE_HACKATHON          => 'campaignevents-eventtype-hackathon',
-		self::EVENT_TYPE_CONFERENCE         => 'campaignevents-eventtype-conference',
-		self::EVENT_TYPE_OTHER              => 'campaignevents-eventtype-other',
-	];
+	public const EVENT_TYPE_OTHER = 'other';
 
-	public const DEBUG_NAMES_MAP = [
-		self::EVENT_TYPE_EDITING_EVENT      => 'editing_event',
-		self::EVENT_TYPE_MEDIA_UPLOAD_EVENT => 'media_upload_event',
-		self::EVENT_TYPE_BACKLOG_DRIVE      => 'backlog_drive',
-		self::EVENT_TYPE_CONTEST            => 'contest',
-		self::EVENT_TYPE_WORKSHOP           => 'workshop',
-		self::EVENT_TYPE_TRAINING           => 'training',
-		self::EVENT_TYPE_MEETUP             => 'meetup',
-		self::EVENT_TYPE_HACKATHON          => 'hackathon',
-		self::EVENT_TYPE_CONFERENCE         => 'conference',
-		self::EVENT_TYPE_OTHER              => 'other',
-	];
-
-	public const EVENT_GROUP_TYPE_MSG_MAP = [
-		self::EVENT_GROUP_TYPE_CONTRIBUTIONS => 'campaignevents-eventtypegroup-contributions',
-		self::EVENT_GROUP_TYPE_COMMUNITY => 'campaignevents-eventtypegroup-community',
-	];
-
-	public const DEBUG_EVENT_GROUP_TYPE_NAMES_MAP = [
-		self::EVENT_GROUP_TYPE_CONTRIBUTIONS => 'contributions',
-		self::EVENT_GROUP_TYPE_COMMUNITY => 'community',
-	];
-
-	// Event type constants
-	public const EVENT_TYPE_EDITING_EVENT      = 'event_type_editing_event';
-	public const EVENT_TYPE_MEDIA_UPLOAD_EVENT = 'event_type_media_upload_event';
-	public const EVENT_TYPE_BACKLOG_DRIVE      = 'event_type_backlog_drive';
-	public const EVENT_TYPE_CONTEST            = 'event_type_contest';
-	public const EVENT_TYPE_WORKSHOP           = 'event_type_workshop';
-	public const EVENT_TYPE_TRAINING           = 'event_type_training';
-	public const EVENT_TYPE_MEETUP             = 'event_type_meetup';
-	public const EVENT_TYPE_HACKATHON          = 'event_type_hackathon';
-	public const EVENT_TYPE_CONFERENCE         = 'event_type_conference';
-	public const EVENT_TYPE_OTHER              = 'event_type_other';
-
-	public const EVENT_GROUP_TYPE_CONTRIBUTIONS = 'contributions';
-	public const EVENT_GROUP_TYPE_COMMUNITY = 'community';
-
-	// Event type groups
-	public const EVENT_TYPE_GROUPS = [
-		self::EVENT_GROUP_TYPE_CONTRIBUTIONS => [
-			self::EVENT_TYPE_EDITING_EVENT,
-			self::EVENT_TYPE_MEDIA_UPLOAD_EVENT,
-			self::EVENT_TYPE_BACKLOG_DRIVE,
-			self::EVENT_TYPE_CONTEST,
+	public const EVENT_TYPES = [
+		[
+			'group' => 'contributions',
+			'msgKey' => 'campaignevents-eventtypegroup-contributions',
+			'types' => [
+				[
+					'type' => 'editing-event',
+					'msgKey'  => 'campaignevents-eventtype-editing-event',
+					'dbValue' => 1 << 0,
+				],
+				[
+					'type' => 'media-upload-event',
+					'msgKey'  => 'campaignevents-eventtype-media-upload-event',
+					'dbValue' => 1 << 1,
+				],
+				[
+					'type' => 'backlog-drive',
+					'msgKey'  => 'campaignevents-eventtype-backlog-drive',
+					'dbValue' => 1 << 2,
+				],
+				[
+					'type' => 'contest',
+					'msgKey'  => 'campaignevents-eventtype-contest',
+					'dbValue' => 1 << 3,
+				],
+			]
 		],
-		self::EVENT_GROUP_TYPE_COMMUNITY => [
-			self::EVENT_TYPE_WORKSHOP,
-			self::EVENT_TYPE_TRAINING,
-			self::EVENT_TYPE_MEETUP,
-			self::EVENT_TYPE_HACKATHON,
-			self::EVENT_TYPE_CONFERENCE,
+		[
+			'group' => 'community',
+			'msgKey' => 'campaignevents-eventtypegroup-community',
+			'types' => [
+				[
+					'type' => 'workshop',
+					'msgKey'  => 'campaignevents-eventtype-workshop',
+					'dbValue' => 1 << 4,
+				],
+				[
+					'type' => 'training',
+					'msgKey'  => 'campaignevents-eventtype-training',
+					'dbValue' => 1 << 5,
+				],
+				[
+					'type' => 'meetup',
+					'msgKey'  => 'campaignevents-eventtype-meetup',
+					'dbValue' => 1 << 6,
+				],
+				[
+					'type' => 'hackathon',
+					'msgKey'  => 'campaignevents-eventtype-hackathon',
+					'dbValue' => 1 << 7,
+				],
+				[
+					'type' => 'conference',
+					'msgKey'  => 'campaignevents-eventtype-conference',
+					'dbValue' => 1 << 8,
+				],
+			]
 		],
-	];
-
-	// Bitwise map (OTHER is 0 and exclusive)
-	public const EVENT_TYPES_MAP = [
-		self::EVENT_TYPE_OTHER              => 0,
-		self::EVENT_TYPE_EDITING_EVENT      => 1,
-		self::EVENT_TYPE_MEDIA_UPLOAD_EVENT => 1 << 1,
-		self::EVENT_TYPE_BACKLOG_DRIVE      => 1 << 2,
-		self::EVENT_TYPE_CONTEST            => 1 << 3,
-		self::EVENT_TYPE_WORKSHOP           => 1 << 4,
-		self::EVENT_TYPE_TRAINING           => 1 << 5,
-		self::EVENT_TYPE_MEETUP             => 1 << 6,
-		self::EVENT_TYPE_HACKATHON          => 1 << 7,
-		self::EVENT_TYPE_CONFERENCE         => 1 << 8,
+		[
+			'group' => 'other',
+			'msgKey' => 'campaignevents-edit-field-eventtypes-other-section-header',
+			'types' => [
+				[
+					'type' => self::EVENT_TYPE_OTHER,
+					'msgKey'  => 'campaignevents-eventtype-other',
+					'dbValue' => 0,
+				],
+			],
+		],
 	];
 
 	private IMessageFormatterFactory $messageFormatterFactory;
@@ -100,8 +91,13 @@ class EventTypesRegistry {
 		$this->messageFormatterFactory = $messageFormatterFactory;
 	}
 
+	/** @return list<string> */
 	public function getAllTypes(): array {
-		return array_keys( self::EVENT_TYPES_MAP );
+		$types = [];
+		foreach ( self::EVENT_TYPES as $group ) {
+			$types = array_merge( $types, array_column( $group['types'], 'type' ) );
+		}
+		return $types;
 	}
 
 	/**
@@ -110,70 +106,73 @@ class EventTypesRegistry {
 	 * @return string
 	 */
 	public function getLocalizedEventTypeName( string $eventType, string $languageCode ): string {
-		if ( !isset( self::EVENT_TYPE_MSG_MAP[$eventType] ) ) {
-			throw new InvalidArgumentException( "Invalid event type: $eventType" );
-		}
-		$formatter = $this->messageFormatterFactory->getTextFormatter( $languageCode );
-		return $formatter->format( MessageValue::new( self::EVENT_TYPE_MSG_MAP[$eventType] ) );
-	}
-
-	/**
-	 * @param string $eventGroupType One of the self::EVENT_GROUP_TYPE_* constants
-	 * @param string $languageCode
-	 * @return string
-	 */
-	public function getLocalizedGroupTypeName( string $eventGroupType, string $languageCode ): string {
-		if ( !isset( self::EVENT_GROUP_TYPE_MSG_MAP[$eventGroupType] ) ) {
-			throw new InvalidArgumentException( "Invalid event group type: $eventGroupType" );
-		}
-		$formatter = $this->messageFormatterFactory->getTextFormatter( $languageCode );
-		return $formatter->format( MessageValue::new( self::EVENT_GROUP_TYPE_MSG_MAP[$eventGroupType] ) );
-	}
-
-	/**
-	 * @param string $eventType One of the self::EVENT_TYPE_* constants
-	 * @return string
-	 */
-	public function getEventTypeDebugName( string $eventType ): string {
-		if ( !isset( self::DEBUG_NAMES_MAP[$eventType] ) ) {
-			throw new InvalidArgumentException( "Invalid event type: $eventType" );
-		}
-		return self::DEBUG_NAMES_MAP[$eventType];
-	}
-
-	/**
-	 * @param string $eventGroupType One of the self::EVENT_GROUP_TYPE_* constants
-	 * @return string
-	 */
-	public function getEventTypeGroupsDebugName( string $eventGroupType ): string {
-		if ( !isset( self::DEBUG_EVENT_GROUP_TYPE_NAMES_MAP[$eventGroupType] ) ) {
-			throw new InvalidArgumentException( "Invalid event type group: $eventGroupType" );
-		}
-		return self::DEBUG_EVENT_GROUP_TYPE_NAMES_MAP[$eventGroupType];
-	}
-
-	/**
-	 * @return array
-	 */
-	public function getAllOptionMessages() {
-		$optionsMessages = [];
-		foreach ( self::EVENT_TYPE_GROUPS as $groupType => $eventTypes ) {
-			$groupLabelMsgKey = self::EVENT_GROUP_TYPE_MSG_MAP[$groupType];
-			$optionsMessages[$groupLabelMsgKey] = [];
-
-			foreach ( $eventTypes as $eventType ) {
-				$eventTypeMsgKey = self::EVENT_TYPE_MSG_MAP[$eventType];
-				$optionsMessages[$groupLabelMsgKey][$eventTypeMsgKey] = $eventType;
+		foreach ( self::EVENT_TYPES as $group ) {
+			foreach ( $group['types'] as $typeInfo ) {
+				if ( $typeInfo['type'] === $eventType ) {
+					$formatter = $this->messageFormatterFactory->getTextFormatter( $languageCode );
+					return $formatter->format( MessageValue::new( $typeInfo['msgKey'] ) );
+				}
 			}
 		}
-		$otherSectionLabelMsgKey = 'campaignevents-edit-field-eventtypes-other-section-header';
-		$otherType = self::EVENT_TYPE_OTHER;
-		$otherTypeMsgKey = self::EVENT_TYPE_MSG_MAP[$otherType];
+		throw new InvalidArgumentException( "Invalid event type: $eventType" );
+	}
 
-		$optionsMessages[$otherSectionLabelMsgKey] = [
-			$otherTypeMsgKey => $otherType,
-		];
+	/**
+	 * @return array<string,array<string,string>> That maps message keys to topic IDs,
+	 *  with category groups represented as nested array of <string,string> suitable for use
+	 *  in multiselect widgets.
+	 */
+	public function getAllOptionMessages(): array {
+		$optionsMessages = [];
+		foreach ( self::EVENT_TYPES as $group ) {
+			$groupLabelMsgKey = $group['msgKey'];
+			$optionsMessages[$groupLabelMsgKey] = [];
 
+			foreach ( $group['types'] as $typeInfo ) {
+				$eventTypeMsgKey = $typeInfo['msgKey'];
+				$optionsMessages[$groupLabelMsgKey][$eventTypeMsgKey] = $typeInfo['type'];
+			}
+		}
 		return $optionsMessages;
+	}
+
+	/**
+	 * Converts event types as stored in the DB into a list of human-readable type identifiers (NOT localized).
+	 * @param string $rawDBEventType
+	 * @return list<string>
+	 */
+	public static function getEventTypesFromDBVal( string $rawDBEventType ): array {
+		$dbEventType = (int)$rawDBEventType;
+		$result = [];
+		foreach ( self::EVENT_TYPES as $group ) {
+			foreach ( $group['types'] as $typeInfo ) {
+				$curDBVal = $typeInfo['dbValue'];
+				if (
+					( $curDBVal === 0 && $dbEventType === 0 ) ||
+					( $curDBVal !== 0 && ( $dbEventType & $curDBVal ) === $curDBVal )
+				) {
+					$result[] = $typeInfo['type'];
+				}
+			}
+		}
+		return $result;
+	}
+
+	/**
+	 * Converts a list of human-readable event type identifiers (NOT localized) to a combination of numeric constants
+	 * that can be stored into the DB.
+	 * @param list<string> $eventTypes
+	 * @return int
+	 */
+	public static function eventTypesToDBVal( array $eventTypes ): int {
+		$dbEventType = 0;
+		foreach ( self::EVENT_TYPES as $group ) {
+			foreach ( $group['types'] as $typeInfo ) {
+				if ( in_array( $typeInfo['type'], $eventTypes, true ) ) {
+					$dbEventType |= $typeInfo['dbValue'];
+				}
+			}
+		}
+		return $dbEventType;
 	}
 }
