7f7c32cf2a6ee81c0fd9be0ef834d642bfa801eb
Daimona Eaytoy <daimona.wiki@gmail.com>
2025-06-04 00:21:40 +0200

    Restrict event page decoration to currently allowed namespaces
    For performance reasons, we can't make DB queries on each and every
    page view without more caching than we already have. So, restrict event
    page decoration to namespaces that are currently allowed. Note this
    limitation in the code.
    
    Bug: T392784
    Change-Id: I6975a6ec91f8e2a11b8eb2d27399ae888241d964

--- src/Hooks/Handlers/ArticleViewHeaderHandler.php
+++ src/Hooks/Handlers/ArticleViewHeaderHandler.php
@@ -4,17 +4,20 @@ declare( strict_types=1 );
 
 namespace MediaWiki\Extension\CampaignEvents\Hooks\Handlers;
 
+use MediaWiki\Config\Config;
 use MediaWiki\Extension\CampaignEvents\EventPage\EventPageDecoratorFactory;
 use MediaWiki\Page\Hook\ArticleViewHeaderHook;
 
 class ArticleViewHeaderHandler implements ArticleViewHeaderHook {
 	private EventPageDecoratorFactory $eventPageDecoratorFactory;
+	private Config $wikiConfig;
 
-	/**
-	 * @param EventPageDecoratorFactory $eventPageDecoratorFactory
-	 */
-	public function __construct( EventPageDecoratorFactory $eventPageDecoratorFactory ) {
+	public function __construct(
+		EventPageDecoratorFactory $eventPageDecoratorFactory,
+		Config $wikiConfig
+	) {
 		$this->eventPageDecoratorFactory = $eventPageDecoratorFactory;
+		$this->wikiConfig = $wikiConfig;
 	}
 
 	/**
@@ -23,6 +26,14 @@ class ArticleViewHeaderHandler implements ArticleViewHeaderHook {
 	public function onArticleViewHeader( $article, &$outputDone, &$pcache ): void {
 		$wikiPage = $article->getPage();
 		if ( !$wikiPage->exists() || $wikiPage->isRedirect() ) {
+			return;
+		}
+
+		// For performance, do not query the database if the namespace is no longer allowed. This will effectively
+		// make event registration disappear from those pages. However, the alternative of making a DB query on
+		// each and every page view is just not sustainable without clever caching strategies, see T392784 and
+		// T392784#10773243 in particular. We can reconsider this later, if needed.
+		if ( !in_array( $wikiPage->getNamespace(), $this->wikiConfig->get( 'CampaignEventsEventNamespaces' ), true ) ) {
 			return;
 		}
 
