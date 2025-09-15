03ed5a8e1d9261beb829f6210b3e3c1f9b41202f
Antoine Musso <hashar@free.fr>
2025-09-15 21:45:18 +0200

    Use lower case 'cldr' to check if it is loaded
    Bug: T398224
    Change-Id: I17ebef8f6041d4bc1cc33cc3b7bc126fa8ed584a

--- src/Hooks/Handlers/ExtensionFunctionHandler.php
+++ src/Hooks/Handlers/ExtensionFunctionHandler.php
@@ -11,8 +11,8 @@ class ExtensionFunctionHandler {
 	public static function checkCLDRIsInstalled(): void {
 		$registry = ExtensionRegistry::getInstance();
 		// T398224 - We can't declare it as a required extension in extension.json
-		if ( !$registry->isLoaded( 'CLDR' ) ) {
-			throw new RuntimeException( 'CLDR is NOT installed' );
+		if ( !( $registry->isLoaded( 'cldr' ) || $registry->isLoaded( 'CLDR' ) ) ) {
+			throw new RuntimeException( 'The cldr extension is NOT installed' );
 		}
 	}
 }
