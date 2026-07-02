0206c2923ee9396f2deda8d150ca9d8cac8272b2
Michelle Horsey <mhorsey@wikimedia.org>
2026-07-02 16:42:39 +0100

    Add event discovery opt-out preference
    Bug: T423741
    Change-Id: I464b3f9ca528ccb3ada2c8e725a5c50529b94c40

--- src/Hooks/Handlers/GetPreferencesHandler.php
+++ src/Hooks/Handlers/GetPreferencesHandler.php
@@ -2,11 +2,18 @@
 declare( strict_types=1 );
 namespace MediaWiki\Extension\CampaignEvents\Hooks\Handlers;
 
+use MediaWiki\Config\Config;
 use MediaWiki\Preferences\Hook\GetPreferencesHook;
 use MediaWiki\User\User;
 
 class GetPreferencesHandler implements GetPreferencesHook {
 	public const ALLOW_INVITATIONS_PREFERENCE = 'campaignevents-allow-invitations';
+	public const OPT_OUT_EVENT_DISCOVERY_PREFERENCE = 'campaignevents-opt-out-event-discovery';
+
+	public function __construct(
+		private readonly Config $config,
+	) {
+	}
 
 	/**
 	 * @param User $user
@@ -16,8 +23,16 @@ class GetPreferencesHandler implements GetPreferencesHook {
 		$preferences[self::ALLOW_INVITATIONS_PREFERENCE] = [
 			'type' => 'toggle',
 			'label-message' => 'campaignevents-invitationlist-preference',
-			// Message: prefs-campaignevents-invitations
-			'section' => 'personal/campaignevents-invitations'
+			// Message: prefs-campaignevents-event-discovery
+			'section' => 'personal/campaignevents-event-discovery'
 		];
+		if ( $this->config->get( 'CampaignEventsEnableWorklists' ) ) {
+			$preferences[self::OPT_OUT_EVENT_DISCOVERY_PREFERENCE] = [
+				'type' => 'toggle',
+				'label-message' => 'campaignevents-eventdiscovery-preference-label',
+				// Message: prefs-campaignevents-event-discovery
+				'section' => 'personal/campaignevents-event-discovery'
+			];
+		}
 	}
 }
